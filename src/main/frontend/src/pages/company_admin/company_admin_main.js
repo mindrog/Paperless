605eb74ade92ab7e40d2c0d4eeee0b81bd6d4eae
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-10-10 17:27:33 +0900

    feat: company_admin main 화면 초기 구현
    feat: company_admin main 화면 초기 구현

--- src/main/frontend/src/pages/company_admin/company_admin_main.js
+++ src/main/frontend/src/pages/company_admin/company_admin_main.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function Company_admin_main() {
     return (
-        <div>
-            <p>기업 관리자 메인 페이지 입니다.</p>
-        </div>
+        <div class="container-xl">100% wide until extra large breakpoint</div>
     );
 }
 
