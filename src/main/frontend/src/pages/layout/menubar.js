4ea23b0f7ff027953acbd52195901deaba1fb2cc
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-10-11 13:56:15 +0900

    chore : company_admin main
    chore : company_admin main
    - manubar create

---
+++ src/main/frontend/src/pages/layout/menubar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const menubar = () => {
+    return (
+        <div class="container">
+        <div class="menubar">
+            <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            </ul>
+        </div>
+        <div class="content">
+            <p>Welcome to the website!</p>
+        </div>
+        </div>
+    )
+}
+
+export default menubar;
