b1e3bcfa25e5e3935d1f34c629a7df7d82355b20
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-10-11 13:56:15 +0900

    chore : company_admin main
    chore : company_admin main
    - manubar create

--- src/main/frontend/src/pages/company_user/company_user_main.js
+++ src/main/frontend/src/pages/company_user/company_user_main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+// import Menubar from '../layout/menubar';
 
 function Company_user_main () {
     return (
         <div>
+            {/* <Menubar /> */}
             <p>기업 사용자 메인 페이지 입니다.</p>
         </div>
     );
