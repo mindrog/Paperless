23c3e8d826446c8aac0e551e786283bb80d91cc3
alsehdns <alsehdns010@gmail.com>
2024-10-11 17:29:14 +0900

    header 구현 1차

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,7 @@
-// src/main/frontend/src/App.js
-
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from './pages/layout/header';
 import Footer from './pages/layout/footer';
-import Menubar from './pages/layout/menubar';
 import Home from './pages/layout/home';
 import Inquiry from './pages/common/inquiry';
 import Login from './pages/layout/login';
@@ -30,55 +27,67 @@ import Company_user_chat from './pages/company_user/company_user_chat';
 import './App.css';
 import './styles/style.css';
 
-import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
-   const [hello, setHello] = useState('')
+    const [hello, setHello] = useState('');
 
     useEffect(() => {
         axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error));
     }, []);
 
     return (
-        <div className='App'>
-            <div className='Header'>
-                <Header />
-            </div>
-            {/* <Menubar /> */}
+        <Router>
+            <div className='App'>
+                <div className='Header'>
+                    <Header />
+                </div>
                 <main className='app'>
-                    <Router>
-                        <Routes>
-                            <Route path='/' element={<Home />} />
-                            <Route path='/inquiry' element={<Inquiry />} />
-                            <Route path='/inquiry/success' element={<Inquiry_Success />} />
-                            <Route path='/Login' element={<Login />} />
-                            <Route path='/Email_Auth' element={<Email_Auth />} />
-                            <Route path='/inquiry/wirte' element={<Inquiry_write/>}/>
-                            <Route path='/System/admin/inquiry' element={<System_admin_inquiry />} />
-                            <Route path='/System/admin/member' element={<System_admin_member />} />
-                            <Route path='/Company/admin/' element={<Company_admin_main />} />
-                            <Route path='/Company/admin/member' element={<Company_admin_memeber />} />
-                            <Route path='/Company/admin/email' element={<Company_admin_email />} />
-                            <Route path='/Company/admin/email/send' element={<Company_admin_email_send />} />
-                            <Route path='/Company/admin/approval' element={<Company_admin_approval />} />
-                            <Route path='/Company/admin/approval/detail' element={<Company_admin_approval_detail />} />
-                            <Route path='/Company/user/' element={<Company_user_main />} />
-                            <Route path='/Company/user/mypage' element={<Company_user_mypage />} />
-                            <Route path='/Company/user/email' element={<Company_user_email />} />
-                            <Route path='/Company/user/email/send' element={<Company_user_email_send />} />
-                            <Route path='/Company/user/draft/list' element={<Company_user_dratf_list />} />
-                            <Route path='/Company/user/draft/detail' element={<Company_user_draft_detail />} />
-                            <Route path='/Company/user/draft/write' element={<Company_user_draft_write />} />
-                            <Route path='/Company/user/chat' element={<Company_user_chat />} />  
-                        </Routes>
-                    </Router>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/inquiry' element={<Inquiry />} />
+                        <Route path='/inquiry/success' element={<Inquiry_Success />} />
+                        <Route path='/Login' element={<Login />} />
+                        <Route path='/Email_Auth' element={<Email_Auth />} />
+                        <Route path='/inquiry/wirte' element={<Inquiry_write />} />
+                        <Route path='/System/admin/inquiry' element={<System_admin_inquiry />} />
+                        <Route path='/System/admin/member' element={<System_admin_member />} />
+                        <Route path='/Company/admin/' element={<Company_admin_main />} />
+                        <Route path='/Company/admin/member' element={<Company_admin_memeber />} />
+                        <Route path='/Company/admin/email' element={<Company_admin_email />} />
+                        <Route path='/Company/admin/email/send' element={<Company_admin_email_send />} />
+                        <Route path='/Company/admin/approval' element={<Company_admin_approval />} />
+                        <Route path='/Company/admin/approval/detail' element={<Company_admin_approval_detail />} />
+                        <Route path='/Company/user/' element={<Company_user_main />} />
+                        <Route path='/Company/user/mypage' element={<Company_user_mypage />} />
+                        <Route path='/Company/user/email' element={<Company_user_email />} />
+                        <Route path='/Company/user/email/send' element={<Company_user_email_send />} />
+                        <Route path='/Company/user/draft/list' element={<Company_user_dratf_list />} />
+                        <Route path='/Company/user/draft/detail' element={<Company_user_draft_detail />} />
+                        <Route path='/Company/user/draft/write' element={<Company_user_draft_write />} />
+                        <Route path='/Company/user/chat' element={<Company_user_chat />} />
+                    </Routes>
                 </main>
-            <div className='Footer'>
-            <Footer />
+                <FooterToggle />
             </div>
-        </div>
+        </Router>
+    );
+}
+
+function FooterToggle() {
+    const uselocation = useLocation();
+    const FooterPaths = ['/', '/inquiry', '/inquiry/success', '/Login', '/Email_Auth', '/inquiry/wirte'];
+
+    return (
+        <>
+            {FooterPaths.includes(uselocation.pathname) && (
+                <div className='Footer'>
+                    <Footer />
+                </div>
+            )}
+        </>
     );
 }
 
--- src/main/frontend/src/pages/layout/footer.js
+++ src/main/frontend/src/pages/layout/footer.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import './layout.css'
-const footer = () => {
+import { useNavigate } from 'react-router-dom';
+import './layout.css';
+
+const Footer = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <footer>
             <div className='footer_btn_container'>
-               <button className='footer_btn'>개인정보 처리 방침</button>
-               <button className='footer_btn'>이용 약관</button>
-               <button className='footer_btn'>문의하기</button>
+                <button className='footer_btn' onClick={() => handleNavigation('/')}>개인정보 처리 방침</button>
+                <button className='footer_btn' onClick={() => handleNavigation('/')}>이용 약관</button>
+                <button className='footer_btn' onClick={() => handleNavigation('/inquiry')}>문의하기</button>
             </div>
-            <div className='footer_compant_info_container'>
-                <div className='footer_compant_info_compname'>
+            <div className='footer_company_info_container'>
+                <div className='footer_company_info_compname'>
                     ㈜페이퍼리스
                 </div>
-                <div className='footer_compant_info'>
+                <div className='footer_company_info'>
                     사업자 등록번호 : 999-99-99999 대표 : 최민경
                 </div>
-                <div className='footer_compant_info'>
+                <div className='footer_company_info'>
                     06626 서울시 서초구 서초대로74길 45 엔데버타워 3층
                 </div>
             </div>
         </footer>
-    )
+    );
 }
 
-export default footer;
+export default Footer;
