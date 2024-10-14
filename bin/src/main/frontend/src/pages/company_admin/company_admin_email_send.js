dff6b112843f5f72c1da44a25b65bdad187fc92b
ocy9911 <dhcksdid99@gmail.com>
2024-10-14 17:50:32 +0900

    email send demo

---
+++ bin/src/main/frontend/src/pages/company_admin/company_admin_email_send.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Company_admin_email_send() {
+    return (
+        <div>
+            <p>기업 관리자 이메일 작성 페이지 입니다.</p>
+        </div>
+    );
+}
+
+export default Company_admin_email_send ;
---
+++ bin/src/main/frontend/src/pages/company_admin/company_admin_member.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Company_admin_member() {
+    return (
+        <div>
+            <p>기업 관리자 회원관리 페이지 입니다.</p>
+        </div>
+    );
+}
+
+export default Company_admin_member ;
