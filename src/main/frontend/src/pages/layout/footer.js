ecfa55b67ce08d97fc083eb375b73edb890462e1
alsehdns <alsehdns010@gmail.com>
2024-10-15 12:09:53 +0900

    main content 2차

--- src/main/frontend/src/pages/layout/footer.js
+++ src/main/frontend/src/pages/layout/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './layout.css';
+import styles from '../../styles/layout/layout.css';
 
 const Footer = () => {
     const navigate = useNavigate();
