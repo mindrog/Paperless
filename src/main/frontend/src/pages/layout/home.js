222a354bf0f36e7260abbda0eca4784bd9be92ba
alsehdns <alsehdns010@gmail.com>
2024-10-15 17:50:34 +0900

    main 3차

--- src/main/frontend/src/pages/layout/home.js
+++ src/main/frontend/src/pages/layout/home.js
@@ -1,6 +1,19 @@
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import main1 from '../../img/main_content1.png';
-import styles from '../../styles/layout/home.css';
+import logo1 from '../../img/logo1.png';
+import logo2 from '../../img/logo2.png';
+import logo3 from '../../img/logo3.png';
+import logo4 from '../../img/logo4.png';
+import logo5 from '../../img/logo5.png';
+import logo6 from '../../img/logo6.png';
+import logo7 from '../../img/logo7.png';
+import logo8 from '../../img/logo8.png';
+import logo9 from '../../img/logo9.png';
+import logo10 from '../../img/logo10.png';
+import logo11 from '../../img/logo11.png';
+import logo12 from '../../img/logo12.png';
+
+import '../../styles/layout/home.css';
 
 const getScrollPosition = () => {
     const scrollPosition = window.scrollY || window.pageYOffset;
@@ -17,7 +30,7 @@ const scrollToTop = () => {
 const scrollToNext = () => {
     window.scrollTo({ top: 868, behavior: 'smooth' });
 }
-
+const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11, logo12];
 function Home() {
     const [isVisible1, setIsVisible1] = useState(false);
     const [isVisible2, setIsVisible2] = useState(false);
@@ -31,32 +44,32 @@ function Home() {
         // 특정 스크롤 위치에 도달했을 때 이미지를 표시
         if (scrollPosition > 828) { // 원하는 스크롤 위치 (예: 200px)
             setIsVisible1(true);
-            
-            } else {
+
+        } else {
             setIsVisible1(false);
-            
+
         }
 
         if (scrollPosition > 1000) { // 원하는 스크롤 위치 (예: 200px)
             setIsVisible2(true);
-            
-            } else {
+
+        } else {
             setIsVisible2(false);
-            
+
         }
-        if (scrollPosition > 2000) { // 원하는 스크롤 위치 (예: 200px)
+        if (scrollPosition > 1300) { // 원하는 스크롤 위치 (예: 200px)
             setIsVisible3(true);
-            
-            } else {
+
+        } else {
             setIsVisible3(false);
-            
+
         }
-        if (scrollPosition > 2500) { // 원하는 스크롤 위치 (예: 200px)
+        if (scrollPosition > 1600) { // 원하는 스크롤 위치 (예: 200px)
             setIsVisible4(true);
-            
-            } else {
+
+        } else {
             setIsVisible4(false);
-            
+
         }
     };
 
@@ -71,7 +84,7 @@ function Home() {
         <div className='main_cont'>
             <div className='cont1_bg'>
                 <div className='main_cont_1'>
-                
+
                     <div className='main_cont_1_title_type1'>
                         <p className='main_cont_1_title_type1_text'>당신의 일이 쉬워질 수 있도록</p>
                     </div>
@@ -90,38 +103,64 @@ function Home() {
                     <div className='top_btn_box'>
                         <button type='button' className='next_div_btn' onClick={scrollToNext}></button>
                         <button type='button' className='top_btn' onClick={scrollToTop}>top</button>
-                    </div>  
+                    </div>
                 </div>
             </div>
             <div className='main_cont_2'>
-                 <div className={`main_cont_2_type1 ${isVisible1 ? 'show' : ''}`}>
+                <div className={`main_cont_2_type1 ${isVisible1 ? 'show' : ''}`}>
                     <div className='type1_img_cont'>
                         <img src={main1} className='type1' alt='Description'></img>
                     </div>
                     <div className='type1_img_script'>
-                        <p className='type1_subtitle'>기능에 대한 제목입니다.</p>
-                        <p className='type1_content'> 기능기능기능기능</p>
+                        <p className='main_script_title'>기능에 대한 제목입니다.</p>
+                        <p className='main_script_contents'> 기능기능기능기능</p>
                     </div>
                 </div>
                 <div className={`main_cont_2_type2 ${isVisible2 ? 'show' : ''}`}>
-                <div className='type2_img_script'>
-                        <p className='type2_subtitle'>기능에 대한 제목입니다.</p>
-                        <p className='type2_content'> 기능기능기능기능</p>
+                    <div className='type2_img_script'>
+                        <p className='main_script_title'>보고서 작성 , 수정 부터 결재까지</p>
+                        <p className='main_script_contents'> 기능기능기능기능</p>
                     </div>
                     <div className='type2_img_cont'>
                         <img src={main1} className='type1' alt='Description'></img>
                     </div>
-                    
-                </div>
-                <div className='main_cont_2_type3'>
 
                 </div>
-                <div className='main_cont_2_type4'>
+                <div className={`main_cont_2_type3 ${isVisible3 ? 'show' : ''}`}>
+                    <div className='type3_img_cont'>
+                        <img src={main1} className='type1' alt='Description'></img>
+                    </div>
+                    <div className='type3_img_script'>
+                        <p className='main_script_title'>기능에 대한 제목입니다.</p>
+                        <p className='main_script_contents'> 기능기능기능기능</p>
+                    </div>
+                </div>
+                <div className={`main_cont_2_type4 ${isVisible4 ? 'show' : ''}`}>
+                    <div className='type4_img_script'>
+                        <p className='main_script_title'>기능에 대한 제목입니다.</p>
+                        <p className='main_script_contents'> 기능은 기능입니다. 특히 기능과 기능적인 부분에서 기능스러운 부분이 강조 되며,<br /> 기능을 책임집니다.</p>
+                    </div>
+                    <div className='type4_img_cont'>
+                        <img src={main1} className='type1' alt='Description'></img>
+                    </div>
 
                 </div>
             </div>
+            <div className='main_cont_3_title'>
+                <p className='main_cont_3_title_type1_text'>협력사 소개</p>
+                <p className='main_cont_3_title_type2_text'>이 외에도 많은 협력사들이 Paperless와 함께하고 있습니다.</p>
+            </div>
             <div className='main_cont_3'>
-
+                <div className="logo-slider">
+                    <div className="logo-slider-track">
+                        {/* 로고를 두 번 출력 */}
+                        {logos.concat(logos).map((logo, index) => (
+                            <div className="logo" key={index}>
+                                <img src={logo} alt={`Logo ${index + 1}`} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
             <div className='main_cont_4'>
 
