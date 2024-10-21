10100f7291bc2b807c2dc3f2d9dc4247349468dd
meenmin2123 <min1016alsrud@gmail.com>
2024-10-22 02:17:46 +0900

    chore : 기안작성(구매,보고,근태 신청) 화면 구현
    chore : 기안작성(구매,보고,근태 신청) 화면 구현

--- src/main/frontend/src/pages/company_user/CompanyUserDraftDetail.js
+++ src/main/frontend/src/pages/company_user/CompanyUserDraftDetail.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import CompanyUserDraftWriteWork from '../company_user/CompanyUserDraftWriteWork';
+import CompanyUserDraftWriteAtten from '../company_user/CompanyUserDraftWriteAtten';
+import CompanyUserDraftWritePurc from '../company_user/CompanyUserDraftWritePurc';
 
-function CompanyUserDraftDetail () {
-    return (
-        <div>
-            <p>기업 사용자 기안 상세 페이지 입니다.</p>
-        </div>
-    );
+function CompanyUserDraftDetail() {
+  const location = useLocation(); // 현재 경로를 가져옴
+
+  // 경로에 따라 렌더링할 컴포넌트를 결정
+  const renderForm = () => {
+    switch (location.pathname) {
+      case '/company/user/draft/write/work':
+        return <CompanyUserDraftWriteWork />;
+      case '/company/user/draft/write/attendance':
+        return <CompanyUserDraftWriteAtten />;
+      case '/company/user/draft/write/purchase':
+        return <CompanyUserDraftWritePurc />;
+      default:
+        return <p>올바른 경로가 아닙니다.</p>;
+    }
+  };
+
+  return (
+    <div>
+      {renderForm()}
+    </div>
+  );
 }
 
 export default CompanyUserDraftDetail;
