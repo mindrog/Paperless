85848778b5e64410bf3f734e6cb30f8afab55075
alsehdns <alsehdns010@gmail.com>
2024-10-23 02:10:11 +0900

    security 구현 1차

--- src/main/java/com/ss/paperless/SecurityConfig.java
+++ src/main/java/com/ss/paperless/SecurityConfig.java
@@ -17,10 +17,12 @@ import com.ss.paperless.employee.LoginFilter;
 @EnableWebSecurity
 public class SecurityConfig {
 	private final AuthenticationConfiguration authenticationConfiguration;
-
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+	private final JWTUtil JWTUtil;
+	
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration,JWTUtil JWTUtil) {
 
         this.authenticationConfiguration = authenticationConfiguration;
+        this.JWTUtil = JWTUtil;
     }
 	@Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -30,32 +32,33 @@ public class SecurityConfig {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CSRF 보호 비활성화
-        http.csrf(csrf -> csrf.disable());
+		 // 커스텀 로그인 필터 추가
+	    http.addFilterBefore(new LoginFilter(authenticationManager(authenticationConfiguration), JWTUtil), UsernamePasswordAuthenticationFilter.class);
+		http.csrf(csrf -> csrf.disable());
 
-        // 로그인 페이지 설정
-        http.formLogin(form -> form
-            .loginPage("/login")
-            .permitAll()
-        );
+	    // 로그인 페이지 설정
+	    http.formLogin(form -> form
+	        .loginPage("/login")
+	        .permitAll()
+	    );
 
-        // HTTP Basic 인증 비활성화
-        http.httpBasic(httpBasic -> httpBasic.disable());
+	    // HTTP Basic 인증 비활성화
+	    http.httpBasic(httpBasic -> httpBasic.disable());
 
-        // 요청에 대한 접근 제어 설정
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(new AntPathRequestMatcher("/api/login")).permitAll() // API 로그인 허용
-                .requestMatchers(new AntPathRequestMatcher("/login"),
-                                 new AntPathRequestMatcher("/"),
-                                 new AntPathRequestMatcher("/join")).permitAll() // 모든 사용자 허용
-                .requestMatchers(new AntPathRequestMatcher("/system/admin")).hasRole("SYSTEMADMIN")
-                .requestMatchers(new AntPathRequestMatcher("/company/admin")).hasRole("COMPANYADMIN")
-                .requestMatchers(new AntPathRequestMatcher("/company/user")).hasRole("COMPANYUSER")
-                .anyRequest().authenticated() // 나머지 요청은 인증 필요
-            );
-
-        // 커스텀 로그인 필터 추가
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
+	    // 요청에 대한 접근 제어 설정
+	    http.authorizeRequests(auth -> auth
+	            .requestMatchers(new AntPathRequestMatcher("/api/login")).permitAll() // API 로그인 허용
+	            .requestMatchers(new AntPathRequestMatcher("/login"),
+	                             new AntPathRequestMatcher("/"),
+	                             new AntPathRequestMatcher("/join")).permitAll() // 모든 사용자 허용
+	            .requestMatchers(new AntPathRequestMatcher("/system/admin")).hasRole("SYSTEMADMIN")
+	            .requestMatchers(new AntPathRequestMatcher("/company/admin")).hasRole("COMPANYADMIN")
+	            .requestMatchers(new AntPathRequestMatcher("/company/user")).hasRole("COMPANYUSER")
+	            .anyRequest().authenticated() // 나머지 요청은 인증 필요
+	        );
+	    
+	   
+	    
+	    return http.build();
+	}
 }
