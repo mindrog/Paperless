42826dbfe0fd1f6268b9648510eafa41e7c72cfb
meenmin2123 <min1016alsrud@gmail.com>
2024-10-24 01:00:15 +0900

    chore : draft_detail 페이지 수정
    chore : draft_detail 페이지 수정
    SQL : CREATE TABLE 전체 코드 추가

--- src/main/frontend/src/pages/company_user/CompanyUserDraftFormPurc.js
+++ src/main/frontend/src/pages/company_user/CompanyUserDraftFormPurc.js
@@ -11,43 +11,9 @@ const CompanyUserDraftFormPurc = () => {
   const [productRows, setProductRows] = useState([{ productName: '', specification: '', quantity: 0, unitPrice: 0, totalPrice: 0, note: '' }]); // productRows 정의
   const [files, setFiles] = useState([]); // files 배열 정의
 
-  const handleAddRow = () => {
-    setProductRows([...productRows, { productName: '', specification: '', quantity: 0, unitPrice: 0, totalPrice: 0, note: '' }]);
-  };
-
-  const handleRemoveRow = (index) => {
-    const updatedRows = productRows.filter((_, idx) => idx !== index);
-    setProductRows(updatedRows);
-  };
-
-  const handleRowChange = (index, field, value) => {
-    const updatedRows = [...productRows];
-    updatedRows[index][field] = value;
-    setProductRows(updatedRows);
-  };
-
-  const handleApprLineModal = () => {
-    console.log("결재선 모달 열기");
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const droppedFiles = Array.from(e.dataTransfer.files);
-    setFiles([...files, ...droppedFiles]);
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  const handleRemoveFile = (index) => {
-    const updatedFiles = files.filter((_, idx) => idx !== index);
-    setFiles(updatedFiles);
-  };
-
   return (
     <div className="container">
-      {/* 생략된 코드 */}
+
     </div>
   );
 };
