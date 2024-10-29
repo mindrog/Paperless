953a4664efc3db8c283e70aefd14f52b6828b053
ocy9911 <dhcksdid99@gmail.com>
2024-10-29 14:18:35 +0900

    ocy

--- src/main/java/com/ss/paperless/email/EmailmessageRepository.java
+++ src/main/java/com/ss/paperless/email/EmailmessageRepository.java
@@ -1,8 +1,13 @@
 package com.ss.paperless.email;
 
 import com.ss.paperless.email.Emailmessage;
+import com.ss.paperless.employee.EmployeeEntity;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailmessageRepository extends JpaRepository<Emailmessage, Long> {
-   
+	List<Emailmessage> findByRecipient(EmployeeEntity recipient);
+    List<Emailmessage> findByWriterOrRecipient(EmployeeEntity writer, EmployeeEntity recipient);
 }
