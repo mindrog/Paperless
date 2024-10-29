27d50c546c79236ab99d890cd6ba9188619cfccb
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-10-29 17:52:03 +0900

    draft_wrtie_work 구현 중...

---
+++ src/main/frontend/src/ckEditor/CustomEditor.js
@@ -0,0 +1,107 @@
+// /**
+//  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+//  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+//  */
+
+// // The editor creator to use.
+// import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
+
+// import { Essentials } from '@ckeditor/ckeditor5-essentials';
+// import { CKFinderUploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
+// import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+// import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+// import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+// import { CKBox } from '@ckeditor/ckeditor5-ckbox';
+// import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
+// import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
+// import { Heading } from '@ckeditor/ckeditor5-heading';
+// import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
+// import { Indent } from '@ckeditor/ckeditor5-indent';
+// import { Link } from '@ckeditor/ckeditor5-link';
+// import { List } from '@ckeditor/ckeditor5-list';
+// import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+// import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+// import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+// import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+// import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+// import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+
+// export default class ClassicEditor extends ClassicEditorBase {
+// 	static builtinPlugins = [
+// 		Essentials,
+// 		CKFinderUploadAdapter,
+// 		Autoformat,
+// 		Bold,
+// 		Italic,
+// 		BlockQuote,
+// 		CKBox,
+// 		CKFinder,
+// 		CloudServices,
+// 		EasyImage,
+// 		Heading,
+// 		Image,
+// 		ImageCaption,
+// 		ImageStyle,
+// 		ImageToolbar,
+// 		ImageUpload,
+// 		Indent,
+// 		Link,
+// 		List,
+// 		MediaEmbed,
+// 		Paragraph,
+// 		PasteFromOffice,
+// 		PictureEditing,
+// 		Table,
+// 		TableToolbar,
+// 		TextTransformation
+// 	];
+
+// 	static defaultConfig = {
+// 		toolbar: {
+// 			items: [
+// 				'undo', 'redo',
+// 				'|', 'heading',
+// 				'|', 'bold', 'italic',
+// 				'|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
+// 				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+// 			]
+// 		},
+// 		fontColor: {
+// 			colors: [
+// 				{ color: 'black', label: 'Black' },
+// 				{ color: 'dimgray', label: 'Dim Gray' },
+// 				{ color: 'gray', label: 'Gray' },
+// 				{ color: 'lightgray', label: 'Light Gray' },
+// 				{ color: 'white', label: 'White', hasBorder: true }
+// 			]
+// 		},
+// 		fontBackgroundColor: {
+// 			colors: [
+// 				{ color: 'red', label: 'Red' },
+// 				{ color: 'orange', label: 'Orange' },
+// 				{ color: 'yellow', label: 'Yellow' },
+// 				{ color: 'lightgreen', label: 'Light Green' },
+// 				{ color: 'green', label: 'Green' }
+// 			]
+// 		},
+// 		image: {
+// 			toolbar: [
+// 				'imageStyle:inline',
+// 				'imageStyle:block',
+// 				'imageStyle:side',
+// 				'|',
+// 				'toggleImageCaption',
+// 				'imageTextAlternative'
+// 			]
+// 		},
+// 		table: {
+// 			contentToolbar: [
+// 				'tableColumn',
+// 				'tableRow',
+// 				'mergeTableCells'
+// 			]
+// 		},
+// 		placeholder: '내용을 입력해주세요',
+// 		language: 'ko'
+// 	};
+// }
