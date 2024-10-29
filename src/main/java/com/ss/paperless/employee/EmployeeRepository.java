5ca0702434b895d7c498f86062a6d46c83ea455c
ocy9911 <dhcksdid99@gmail.com>
2024-10-29 14:18:35 +0900

    ocy

--- src/main/java/com/ss/paperless/employee/EmployeeRepository.java
+++ src/main/java/com/ss/paperless/employee/EmployeeRepository.java
@@ -7,9 +7,7 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer>{
 	Boolean existsByEmpCode(String empCode); // emp_code에 맞게 수정
     
-    // emp_code로 직원 조회
+	//찬양 email에서 employee 정보 가져오기
+	EmployeeEntity findByEmpEmail(String empEmail);
     EmployeeEntity findByEmpCode(String empCode);
-    
-    // emp_Email로 이메일 조회
-    EmployeeEntity findByEmpEmail(String empEmail);
 }
