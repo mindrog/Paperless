ee1cd0090a4469b51f4c1b5c194034e90b8865d7
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-10-30 15:25:58 +0900

    draft_write_work 구현
    - 호출 fetch 컴포넌트 구현하여 통신 확인
    - 기안 문서에 emp 정보 띄우기 확인

--- src/main/java/com/ss/paperless/email/EmailmessageRepository.java
+++ src/main/java/com/ss/paperless/email/EmailmessageRepository.java
@@ -1,6 +1,5 @@
 package com.ss.paperless.email;
 
-import com.ss.paperless.employee.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/ss/paperless/report/ReportController.java
+++ src/main/java/com/ss/paperless/report/ReportController.java
@@ -1,11 +1,16 @@
 package com.ss.paperless.report;
 
+import com.ss.paperless.employee.entity.EmployeeEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ss.paperless.employee.EmployeeDTO;
 
 @Slf4j
 @RestController
@@ -13,12 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 public class ReportController {
 
-    @GetMapping("/report/getUserInfo")
-    public String getUserInfo() {
+    @Autowired
+    private ReportService reportService;
+
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<EmployeeDTO> getUserInfo() {
         String emp_code =  SecurityContextHolder.getContext().getAuthentication().getName();
+//        int emp_no = SecurityContextHolder.getContext().getAuthentication().get;
         System.out.println("emp_code : " + emp_code);
 
-        return "getUserInfo";
+        EmployeeEntity userInfo = reportService.getUserInfo(emp_code);
+
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+
+        employeeDTO.setEmp_code(userInfo.getEmpCode());
+        employeeDTO.setEmp_name(userInfo.getEmpName());
+        employeeDTO.setEmp_dept_no(userInfo.getEmpDeptNo());
+        employeeDTO.setDept_name(userInfo.getDepartment().getDeptName());
+        employeeDTO.setDept_team_name(userInfo.getDepartment().getDeptTeamName());
+
+        System.out.println("ResponseEntity content: " + employeeDTO.toString());
+
+        return ResponseEntity.ok(employeeDTO);
     }
 
 }
