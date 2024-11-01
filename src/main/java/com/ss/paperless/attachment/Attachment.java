0a73fa6e8da4137584998ea877d970604e2aaede
ocy9911 <dhcksdid99@gmail.com>
2024-11-01 10:18:20 +0900

    gitinore API KEY 추가

--- src/main/java/com/ss/paperless/attachment/Attachment.java
+++ src/main/java/com/ss/paperless/attachment/Attachment.java
@@ -14,12 +14,16 @@ public class Attachment {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "atta_no")
 	private Long attaNo;
+	
 	@Column(name = "atta_key", nullable = false, length = 500)
 	private String attaKey;
+	
 	@Column(name = "atta_url", nullable = false, length = 1000)
 	private String attaUrl;
+	
 	@Column(name = "atta_original_name", nullable = false, length = 500)
 	private String attaOriginalName;
+	
 	@Column(name = "atta_size", nullable = false)
 	private Long attaSize;
 }
