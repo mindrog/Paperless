6af53927cb9b65cfa01f0891a82ea243b3292329
ocy9911 <dhcksdid99@gmail.com>
2024-11-04 10:38:37 +0900

    env적용

--- src/main/java/com/ss/paperless/PaperlessApplication.java
+++ src/main/java/com/ss/paperless/PaperlessApplication.java
@@ -3,12 +3,14 @@ package com.ss.paperless;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class PaperlessApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(PaperlessApplication.class, args);
 	}
 
