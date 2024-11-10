2b57537dce2f46984fa6fe9c0a7acffee30f5da9
ocy9911 <dhcksdid99@gmail.com>
2024-11-10 18:36:38 +0900

    ocy

--- src/main/java/com/ss/paperless/email/EmailService.java
+++ src/main/java/com/ss/paperless/email/EmailService.java
@@ -210,7 +210,6 @@ public class EmailService {
 
 	public int getUnreadCount(String emp_code) {
 		EmployeeEntity employee=employeeRepository.findByEmpCode(emp_code);
-		
 		return mapper.getUnreadCount(employee.getEmpNo());
 	}
 
